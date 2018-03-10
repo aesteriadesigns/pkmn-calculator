30afdc1ba9d04372baec0f1f9c4a888190766167
Kayla <aesteriadesigns@gmail.com>
2018-03-10 15:22:33 +1000

    Update js.js

--- js.js
+++ js.js
@@ -2,18 +2,20 @@ function calculateDamage() {
 
 	var lvl = document.getElementById("lvl").value;
 	var pwr = document.getElementById("pwr").value;
-	var attdef = document.getElementById("attdef").value;
+	var att = document.getElementById("att").value;
+	var def = document.getElementById("def").value;
 	var d6 = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
 	var mod = document.getElementById("mod").value;
 	var result = document.getElementById("result");
 
 	lvl = parseFloat(lvl);
 	pwr = parseFloat(pwr);
-	attdef = parseFloat(attdef);
+	att = parseFloat(att);
+	def = parseFloat(def);
 	d6 = parseFloat(d6);
 	mod = parseFloat(mod);
 
-	var damage = (((((2 * lvl/5 + 2) * pwr * attdef) / 50) + d6) * mod);
+	var damage = (((((2 * lvl/5 + 2) * pwr * att / def) / 50) + d6) * mod);
 
 	if (isNaN(damage)) {
 		document.getElementById("result").innerHTML = "Error occured. Try again";
