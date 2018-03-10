5094a94d5b2e3431aaddc0bc066d5c0931068fa7
Kayla <aesteriadesigns@gmail.com>
2018-03-10 12:53:05 +1000

    Add files via upload

---
+++ js.js
@@ -0,0 +1,39 @@
+function calculateDamage() {
+
+	var lvl = document.getElementById("lvl").value;
+	var pwr = document.getElementById("pwr").value;
+	var attdef = document.getElementById("attdef").value;
+	var d6 = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+	var mod = document.getElementById("mod").value;
+	var result = document.getElementById("result");
+
+	lvl = parseFloat(lvl);
+	pwr = parseFloat(pwr);
+	attdef = parseFloat(attdef);
+	d6 = parseFloat(d6);
+	mod = parseFloat(mod);
+
+	var damage = (((((2 * lvl/5 + 2) * pwr * attdef) / 50) + d6) * mod);
+
+	if (isNaN(damage)) {
+		document.getElementById("result").innerHTML = "Error occured. Try again";
+		document.getElementById("result").style.display = "block";
+		document.getElementById("d6").value = d6;
+	} else {
+		document.getElementById("result").innerHTML = "Damage Dealt: "  + Math.round(damage);
+		document.getElementById("result").style.display = "block";
+		document.getElementById("d6").value = d6;
+	}
+
+}
+
+function resetForm() {
+	var elements = document.getElementsByTagName("input");
+	for (var ii = 0; ii < elements.length; ii++) {
+		if (elements[ii].type == "number") {
+			elements[ii].value = "";
+			document.getElementById("result").innerHTML = " ";
+			document.getElementById("result").style.display = "none";
+		}
+	}
+}
